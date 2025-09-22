abc5296558aab4efef237fbf534a4c31c826dc0c
bunnystring <ziknaf_548@hotmail.com>
2025-09-21 21:42:11 -0500

    Add infra-orders-service project with basic server configuration

---
+++ src/main/java/com/infragest/infra_orders_service/InfraOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_orders_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraOrdersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraOrdersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infragest/infra_orders_service/InfraOrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_orders_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraOrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
